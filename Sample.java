9161c04a8b40d9884f87c993f1007436ad5afee8
Velvignesh Pillai <141361652+velvignesh6404@users.noreply.github.com>
2025-04-01 00:46:53 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,5 @@
-class HelloWorld
-{
-    public static void main(String[] args)
-    {
-        System.out.println(“Hello, World”);
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
     }
 }
